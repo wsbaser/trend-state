d58fce819f0691e1deb5165874320f17e794ac36
Alexey Volkov <wsbaser@gmail.com>
2018-11-11 19:09:56 +0300

    cleared chart from unnecessary indicators

--- trend-state/ClientApp/src/components/Chart.js
+++ trend-state/ClientApp/src/components/Chart.js
@@ -31,16 +31,6 @@ function getMaxUndefined(calculators) {
 }
 const LENGTH_TO_SHOW = 180;
 
-const macdAppearance = {
-    stroke: {
-        macd: "#FF0000",
-        signal: "#00F300",
-    },
-    fill: {
-        divergence: "#4682B4"
-    },
-};
-
 class CandleStickChartPanToLoadMore extends React.Component {
     constructor(props) {
         super(props);
@@ -58,33 +48,14 @@ class CandleStickChartPanToLoadMore extends React.Component {
             .merge((d, c) => { d.ema12 = c; })
             .accessor(d => d.ema12);
 
-        const macdCalculator = macd()
-            .options({
-                fast: 12,
-                slow: 26,
-                signal: 9,
-            })
-            .merge((d, c) => { d.macd = c; })
-            .accessor(d => d.macd);
-
-        const smaVolume50 = sma()
-            .id(3)
-            .options({
-                windowSize: 50,
-                sourcePath: "volume",
-            })
-            .merge((d, c) => { d.smaVolume50 = c; })
-            .accessor(d => d.smaVolume50);
-
-        const maxWindowSize = getMaxUndefined([ema26,
-            ema12,
-            macdCalculator,
-            smaVolume50
+        const maxWindowSize = getMaxUndefined([
+            ema26,
+            ema12
         ]);
         /* SERVER - START */
         const dataToCalculate = inputData.slice(-LENGTH_TO_SHOW - maxWindowSize);
 
-        const calculatedData = ema26(ema12(macdCalculator(smaVolume50(dataToCalculate))));
+        const calculatedData = ema26(ema12(dataToCalculate));
         const indexCalculator = discontinuousTimeScaleProviderBuilder().indexCalculator();
 
         // console.log(inputData.length, dataToCalculate.length, maxWindowSize)
@@ -101,8 +72,6 @@ class CandleStickChartPanToLoadMore extends React.Component {
         this.state = {
             ema26,
             ema12,
-            macdCalculator,
-            smaVolume50,
             linearData,
             data: linearData,
             xScale,
@@ -122,16 +91,14 @@ class CandleStickChartPanToLoadMore extends React.Component {
         const rowsToDownload = end - Math.ceil(start);
 
         const maxWindowSize = getMaxUndefined([ema26,
-            ema12,
-            macdCalculator,
-            smaVolume50
+            ema12
         ]);
 
         /* SERVER - START */
         const dataToCalculate = inputData
             .slice(-rowsToDownload - maxWindowSize - prevData.length, - prevData.length);
 
-        const calculatedData = ema26(ema12(macdCalculator(smaVolume50(dataToCalculate))));
+        const calculatedData = ema26(ema12(dataToCalculate));
         const indexCalculator = discontinuousTimeScaleProviderBuilder()
             .initialIndex(Math.ceil(start))
             .indexCalculator();
@@ -163,7 +130,7 @@ class CandleStickChartPanToLoadMore extends React.Component {
         const { data, ema26, ema12, macdCalculator, smaVolume50, xScale, xAccessor, displayXAccessor } = this.state;
 
         return (
-            <ChartCanvas ratio={ratio} width={width} height={600}
+            <ChartCanvas ratio={ratio} width={width} height={450}
                 margin={{ left: 70, right: 70, top: 20, bottom: 30 }} type={type}
                 seriesName="MSFT"
                 data={data}
@@ -172,7 +139,8 @@ class CandleStickChartPanToLoadMore extends React.Component {
                 <Chart id={1} height={400}
                     yExtents={[d => [d.high, d.low], ema26.accessor(), ema12.accessor()]}
                     padding={{ top: 10, bottom: 20 }}>
-                    <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
+
+                    <XAxis axisAt="bottom" orient="bottom" outerTickSize={0} />
                     <YAxis axisAt="right" orient="right" ticks={5} />
 
                     <MouseCoordinateY
@@ -187,66 +155,8 @@ class CandleStickChartPanToLoadMore extends React.Component {
                     <CurrentCoordinate yAccessor={ema26.accessor()} fill={ema26.stroke()} />
                     <CurrentCoordinate yAccessor={ema12.accessor()} fill={ema12.stroke()} />
 
-                    <EdgeIndicator itemType="last" orient="right" edgeAt="right"
-                        yAccessor={d => d.close} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"} />
-
-                    <OHLCTooltip origin={[-40, 0]} />
-                    <MovingAverageTooltip
-                        onClick={(e) => console.log(e)}
-                        origin={[-38, 15]}
-                        options={[
-                            {
-                                yAccessor: ema26.accessor(),
-                                type: ema26.type(),
-                                stroke: ema26.stroke(),
-                                ...ema26.options(),
-                            },
-                            {
-                                yAccessor: ema12.accessor(),
-                                type: ema12.type(),
-                                stroke: ema12.stroke(),
-                                ...ema12.options(),
-                            },
-                        ]}
-                    />
                 </Chart>
-                <Chart id={2} height={150}
-                    yExtents={[d => d.volume, smaVolume50.accessor()]}
-                    origin={(w, h) => [0, h - 300]}>
-                    <YAxis axisAt="left" orient="left" ticks={5} tickFormat={format(".2s")} />
 
-                    <MouseCoordinateY
-                        at="left"
-                        orient="left"
-                        displayFormat={format(".4s")} />
-
-                    <BarSeries yAccessor={d => d.volume} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"} />
-                    <AreaSeries yAccessor={smaVolume50.accessor()} stroke={smaVolume50.stroke()} fill={smaVolume50.fill()} />
-                </Chart>
-                <Chart id={3} height={150}
-                    yExtents={macdCalculator.accessor()}
-                    origin={(w, h) => [0, h - 150]} padding={{ top: 10, bottom: 10 }} >
-                    <XAxis axisAt="bottom" orient="bottom" />
-                    <YAxis axisAt="right" orient="right" ticks={2} />
-
-                    <MouseCoordinateX
-                        at="bottom"
-                        orient="bottom"
-                        displayFormat={timeFormat("%Y-%m-%d")} />
-                    <MouseCoordinateY
-                        at="right"
-                        orient="right"
-                        displayFormat={format(".2f")} />
-
-                    <MACDSeries yAccessor={d => d.macd}
-                        {...macdAppearance} />
-                    <MACDTooltip
-                        origin={[-38, 15]}
-                        yAccessor={d => d.macd}
-                        options={macdCalculator.options()}
-                        appearance={macdAppearance}
-                    />
-                </Chart>
                 <CrossHairCursor />
             </ChartCanvas>
         );
