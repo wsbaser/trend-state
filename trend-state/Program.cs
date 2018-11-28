a2134edba83007a0f7df5e5af83afb1e3c216f4c
wsbaser <wsbaser@gmail.com>
2018-11-28 23:17:04 +0300

    sdf

--- trend-state/Program.cs
+++ trend-state/Program.cs
@@ -15,6 +15,24 @@ namespace trend_state
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            int delay = 1;
+            var cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+
+            var listener = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    // poll hardware
+
+
+                    Thread.Sleep(delay);
+                    if (token.IsCancellationRequested)
+                        break;
+                }
+
+                // cleanup, e.g. close connection
+            }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
